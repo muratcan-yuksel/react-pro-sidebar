b7d50ed29c7c452230f95da72f9a247789455e13
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-08 03:34:09 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import "./App.css";
+
 function Layout() {
   const { collapseSidebar } = useProSidebar();
+  // const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div id="app">
+    <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
       <Sidebar id="sidebar" style={{ height: "100%" }}>
         <Menu>
-          <div>lorem</div>
-          <MenuItem>sth Documentation</MenuItem>
-          <MenuItem> Calendar</MenuItem>
-          <MenuItem> E-commerce</MenuItem>
+          <MenuItem
+            icon={<MenuOutlinedIcon />}
+            onClick={() => {
+              collapseSidebar();
+            }}
+            style={{ textAlign: "center" }}
+          >
+            {" "}
+            <h2>Admin</h2>
+          </MenuItem>
+
+          <MenuItem icon={<HomeOutlinedIcon />}>Home</MenuItem>
+          <MenuItem icon={<PeopleOutlinedIcon />}>Team</MenuItem>
+          <MenuItem icon={<ContactsOutlinedIcon />}>Contacts</MenuItem>
+          <MenuItem icon={<ReceiptOutlinedIcon />}>Profile</MenuItem>
+          <MenuItem icon={<HelpOutlineOutlinedIcon />}>FAQ</MenuItem>
+          <MenuItem icon={<CalendarTodayOutlinedIcon />}>Calendar</MenuItem>
         </Menu>
       </Sidebar>
       <main>
-        <button onClick={() => collapseSidebar()}>Collapse</button>
+        <h1 style={{ color: "white", marginLeft: "5rem" }}>
+          React-Pro-Sidebar
+        </h1>
       </main>
     </div>
   );
